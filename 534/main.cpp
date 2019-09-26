70cd71f7b71274d287097972ac013284d8aa3366
Pranav <pranavgupta000@gmail.com>
2019-09-26 12:51:42 +0530

    new functions

--- 534/main.cpp
+++ 534/main.cpp
@@ -1,119 +1,156 @@
-            #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-            using namespace std;
+using namespace std;
+typedef long long lli;
+typedef unsigned long long int uli;
+typedef long double Lf;
 
-            #define fastIO  std::ios::sync_with_stdio(false);cin.tie(NULL)
-            #define uli unsigned long long int
-            #define lli long long int
-            #define Lf long double
-            #define mod 1000000007
-            #define N 51
-            #define NN 21
-            #define vi vector<int>
-            #define vl vector<long>
-            #define vll vector<long long>
-            #define vc vector<char>
-            #define ii <int, int>
-            #define ll <long, long>
-            #define lll <long long, long long>
-            #define loop(i,s,n) for(int i=s;i<n;i++)
-            #define loopr(i,n,s) for(int i=n-1;i>=s;i--)
-            #define pb push_back
+#define fastIO  std::ios::sync_with_stdio(false);cin.tie(NULL)
+#define mod 1000000007
+#define BIGL = 999999999999999999
+#define BIGI = 99999999
+#define N 21
+#define NN 21
+#define vi vector<int>
+#define vl vector<long>
+#define vll vector<long long>
+#define vc vector<char>
+#define ii <int, int>
+#define ll <long, long>
+#define lll <long long, long long>
+#define loop(i,s,n) for(int i=s;i<n;i++)
+#define loopr(i,n,s) for(int i=n-1;i>=s;i--)
+#define pb push_back
 
-            inline lli modadd(lli n,lli m){
-                lli sum = ((n+m)%mod+mod)%mod;
-                return sum;
-            }
-
-            inline lli modsub(lli n,lli m){
-                lli diff = ((n-m+mod)%mod+mod)%mod;
-                return diff;
-            }
-
-            inline lli modpro(lli n,lli m){
-                lli pro = ((n*m)%mod+mod)%mod;
-                return pro;
-            }
-
-            inline lli pow(int a,int x){
-                lli powe = 1;
-                loop(i,0,x){
-                    powe*=a;
-                }
-                return powe;
-            }
-
-        inline lli powmod(int a,int x){
-                lli powe = 1;
-                loop(i,0,x){
-                    powe=modpro(powe,a);
-                }
-                return powe;
-            }
-            int index;
-
-
-int dp[N][N];
-int n, a[N];
-vi v;
-int finds(int mn, int k){
-    if(dp[mn][k]!=-1) return dp[mn][k];
-    if(k>(n-1)){
-            cout<<mn<<" "<<k<<0<<"^"<<endl;
-        return 0;
-    }
-    if(k>0&&(mn==0)){
-        dp[mn][k]=1;
-        cout<<mn<<" "<<k<<1<<endl;
-        return 1;
-    }
-   // int r;
-    //r = (dp[((mn+a[k])%n)][k+1])|(dp[mn][k+1]);
-    if(finds(((mn+a[k])%n),k+1)==1){
-       v.pb(k);
-       cout<<mn<<" "<<k<<1<<endl;
-        dp[mn][k]=1;
-        return 1;
-    }
-    else if(finds(mn,k+1)==1){
-            dp[mn][k]=1;
-            cout<<mn<<" "<<k<<1<<endl;
-        return 1;
-    }
-    dp[mn][k]=0;
-    cout<<mn<<" "<<k<<0<<"@"<<endl;
-    return 0;
-
+inline lli modadd(lli n,lli m){
+    lli sum = ((n+m)%mod+mod)%mod;
+    return sum;
 }
 
+inline lli modsub(lli n,lli m){
+    lli diff = ((n-m+mod)%mod+mod)%mod;
+    return diff;
+}
+
+inline lli modpro(lli n,lli m){
+    lli pro = ((n*m)%mod+mod)%mod;
+    return pro;
+}
+
+
+
+inline lli pow(int x,int y){
+    lli res = 1;
+    while (y > 0) {
+        if (y & 1)
+            res = res * x;
+        y = y >> 1;
+        x = x * x;
+    }
+    return res;
+}
+
+
+
+inline lli powmod(lli x,lli y){
+    lli res = 1;
+    while (y > 0) {
+        if (y & 1)
+            res = powmod(res , x);
+        y = y >> 1;
+        x = powmod(x , x);
+    }
+    return res;
+}
+
+
+
+
+template <class T, class U>
+bool comparep(const pair<T, U>&i, const pair<T, U>&j)
+{
+    return i.first > j.first;
+}
+
+
+
+
+template <typename T>
+T gcd(T a, T b)
+{
+    if (a == 0)
+        return b;
+    if(b==0)
+        return a;
+    T t;
+    while((a>0)&&(b>0)){
+     t = a;
+     a=b%a;
+     b=t;
+ }
+
+ return max(a,b);
+}
+
+
+
+template <typename T>
+T maxof(T n_args, ...)
+{
+    va_list ap;
+    va_start(ap, n_args);
+    T big = va_arg(ap, T);
+    for(int i = 2; i <= n_args; i++) {
+        T a = va_arg(ap, T);
+        if(a > big) big = a;
+    }
+    va_end(ap);
+    return big;
+}
+
+
+
+
+template <typename T>
+T gcdarr(T a[], int n){
+    T gc = gcd<T>(a[0],a[1]);
+    loop(i,2,n) {
+        gc = gcd<T>(gc,a[i]);
+    }
+    return gc;
+}
+
+
+template <typename T>
+T maxarr(T a[], int n){
+    T big = a[0];
+    loop(i,1,n) {
+        big = max(big, a[i]);
+    }
+    return big;
+}
+
+
+
+template <typename T>
+T minarr(T a[], int n){
+    T small = a[0];
+    loop(i,1,n) {
+        small = min(small, a[i]);
+    }
+    return small;
+}
+
+
 int main(){
-   int t;
-   cin>>t;
-   while(t--){
-    cin>>n;
-    loop(i,0,n){
-    cin>>a[i];
-    }
-    memset(dp,-1,sizeof(dp));
-    v.clear();
-    if(finds(0,0)){
-           // sort(v.begin(),v.end());
-        loop(i,0,v.size()){
-            cout<<v[i]+1<<" ";
-        }cout<<endl;
-    }
-    else cout<<-1<<endl;
-   /* loop(i,0,n){
-        loop(j,0,n){
-            cout<<dp[i][j]<<" ";
-        }
-        cout<<endl;
-    }*/
-   }
+    int n,m,x;
+    cin>>n>>m>>x;
 
 
+    cout<<maxof<int>(3,n,m,x)<<endl;;
 
-            return 0;
-            }
+
+    return 0;
+}
 
 
